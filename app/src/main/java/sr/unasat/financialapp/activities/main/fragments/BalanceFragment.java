93b3541f6a9e8f9425ec061d926dad8501271743
JairJosafath <jairjosafath@gmail.com>
2017-02-24 14:29:29 -0300

    remooved datepicker

--- app/src/main/java/sr/unasat/financialapp/activities/main/fragments/BalanceFragment.java
+++ app/src/main/java/sr/unasat/financialapp/activities/main/fragments/BalanceFragment.java
@@ -4,6 +4,7 @@ package sr.unasat.financialapp.activities.main.fragments;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,7 +31,7 @@ import static sr.unasat.financialapp.util.DateUtil.convertDate;
 
 public class BalanceFragment extends Fragment {
 
-    int startyear,startmonth,startday,endyear,endmonth,endday;int counter=0;int counter2=0,counter3=0; Date date ;
+     Date date ;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,7 +40,7 @@ public class BalanceFragment extends Fragment {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_balance, container, false);
 
-        counter = counter2 = counter3=0;
+        //counter = counter2 = counter3=0;
         Dao dao = new Dao(getActivity());
         final String spinnerItem1 ="this month";
         final String spinnerItem2 ="last month";
@@ -52,6 +53,7 @@ public class BalanceFragment extends Fragment {
         spinner.setAdapter(spinnerAdapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
@@ -61,7 +63,7 @@ public class BalanceFragment extends Fragment {
                 switch (String.valueOf(parent.getItemAtPosition(position))){
 
                     case spinnerItem1:
-                        setDays(date,0);
+                        setDays(date,100);
                         break;
                     case spinnerItem2:
                         int month=convertDate(date)[1];
@@ -73,10 +75,9 @@ public class BalanceFragment extends Fragment {
                         break;
 
                     case spinnerItem3:
-                        int[] array = convertDate(date);
-                        openDatePicker(array[0],array[1],array[2]);
 
                         break;
+
 
                 }
 
@@ -101,57 +102,12 @@ public class BalanceFragment extends Fragment {
     }
 
     private void setDays(Date date,int month){
+        getView().findViewById(R.id.group_listView).refreshDrawableState();
         Dao dao=new Dao(getActivity());
 
-        if (month==0&&counter2!=0) {
+                int[] dateArr = convertDate(date);
 
-            List<String> days = new ArrayList<>();
-            int[] years = new int[(endyear-startyear)+1];
-            int nyear=startyear;
-            for (int i=1;i<years.length;i++){
-                years[i]=nyear+1;
-                nyear++;
-            }
-            years[0]=startyear;
-            HashMap<String, List<String>> transactions = new HashMap<>();
-            List<String> tranNames;
-
-            for (int i =1;i<years.length;i++) {
-                for (int year:years) {
-                    List<String> listDays = dao.getDays(i, year);
-
-                    for (String theDay : listDays) {
-
-                        String[] array = theDay.split("\\s");
-                        int day = Integer.valueOf(array[0]);
-                        tranNames = new ArrayList<>();
-                        List<Transaction> list = dao.getTransactionsByDay(day, i, year);
-                        for (Transaction transaction : list) {
-
-                            tranNames.add(transaction.getTran_name());
-                            transactions.put(theDay, tranNames);
-                        }
-                        days.add(theDay);
-                    }
-                }
-            }
-
-
-            ExpandableListView groupListView = (ExpandableListView) getView().findViewById(R.id.group_listView);
-
-            DateGroupExpendableAdapter adapter = new DateGroupExpendableAdapter(days, transactions, getContext());
-
-            groupListView.setAdapter(adapter);
-
-
-        }
-        else {
-
-
-            counter2++;
-            int[] dateArr = convertDate(date);
-
-            if (month == 0) {
+            if (month == 100) {
                 month = dateArr[1];
             }
             List<String> days = dao.getDays(month, dateArr[0]);
@@ -187,6 +143,10 @@ public class BalanceFragment extends Fragment {
             DateGroupExpendableAdapter adapter = new DateGroupExpendableAdapter(days, transactions, getContext());
 
             groupListView.setAdapter(adapter);
+
+
+
+
         }
 
     }
@@ -194,32 +154,8 @@ public class BalanceFragment extends Fragment {
 
 
 
-    private void openDatePicker(int year1,int month1,int day1){
 
-        Dialog startdiag = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                setStartDate(year,month+1,dayOfMonth);
-
-                if (counter>0){
-                    openEndDatePicker(startyear,startmonth,startday);
-                }
-                counter++;
-
-            }
-
-        }, year1, month1, day1);
-        startdiag.setTitle("start date");
-        startdiag.show();
-
-
-}
-
-    private void openEndDatePicker(int year1,int month1,int day1){
-        Dialog diag = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                setEndDate(year,month+1,dayOfMonth);
+/*setEndDate(year,month+1,dayOfMonth);
                 if (counter3!=0) {
 
                     setDays(date, 0);
@@ -227,26 +163,4 @@ public class BalanceFragment extends Fragment {
                 counter3++;
                 if (counter3==2){
                     counter3=0;
-                }
-            }
-        }, year1, month1, day1);
-        diag.setTitle("end date");
-        diag.show();
-    }
-
-
-    void setStartDate(int year, int month, int day){
-        startyear = year;
-        startmonth = month;
-        startday = day;
-
-    }
-    void setEndDate(int year, int month, int day){
-        endyear = year;
-        endmonth = month;
-        endday = day;
-
-    }
-
-
-}
+                }*/
