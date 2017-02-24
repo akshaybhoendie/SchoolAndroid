76d51ddf7d2fdf04ca9f0f9ce9c1a617a65c1401
akshaybhoendie <akshaybhoendie@gmail.com>
2017-02-24 00:33:57 -0300

    - added budget arrayadapter

---
+++ app/src/main/java/sr/unasat/financialapp/arrayadapters/BudgetArrayAdapter.java
@@ -0,0 +1,15 @@
+package sr.unasat.financialapp.arrayadapters;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by abhoendie on 2/23/2017.
+ */
+
+public class BudgetArrayAdapter extends ArrayAdapter <String> {
+
+    public BudgetArrayAdapter(Context context, int resource) {
+        super(context, resource);
+    }
+}
