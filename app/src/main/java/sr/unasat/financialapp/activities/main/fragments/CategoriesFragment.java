db5d6fb2de6854c744810fb07b8f9dab64a4e7e8
JairJosafath <jairjosafath@gmail.com>
2017-02-26 14:44:34 -0300

    balance afronden

--- app/src/main/java/sr/unasat/financialapp/activities/main/fragments/CategoriesFragment.java
+++ app/src/main/java/sr/unasat/financialapp/activities/main/fragments/CategoriesFragment.java
@@ -3,19 +3,51 @@ package sr.unasat.financialapp.activities.main.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import sr.unasat.financialapp.R;
+import sr.unasat.financialapp.arrayadapters.TransactionRecyclerAdapter;
+import sr.unasat.financialapp.db.dao.Dao;
+import sr.unasat.financialapp.dto.Category;
+import sr.unasat.financialapp.dto.Transaction;
 
 public class CategoriesFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private TransactionRecyclerAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories, container, false);
+
+        View view= inflater.inflate(R.layout.fragment_categories, container, false);
+        Dao dao = new Dao(getActivity());
+        List<Category>categories=dao.getCategories();
+        List<String>categoriesStr = new ArrayList<>();
+        for (Category category : categories){
+
+            categoriesStr.add(category.getName());
+
+        }
+
+        recyclerView = (RecyclerView)view.findViewById(R.id.cat_recycler);
+        adapter = new TransactionRecyclerAdapter(categoriesStr);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setAdapter(adapter);
+
+        return view;
+
     }
 
 }
--- app/src/main/java/sr/unasat/financialapp/arrayadapters/TransactionRecyclerAdapter.java
+++ app/src/main/java/sr/unasat/financialapp/arrayadapters/TransactionRecyclerAdapter.java
@@ -1,25 +1,54 @@
 package sr.unasat.financialapp.arrayadapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import sr.unasat.financialapp.R;
 
 /**
  * Created by Jair on 2/26/2017.
  */
 
-public class TransactionRecyclerAdapter extends RecyclerView.Adapter {
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+public class TransactionRecyclerAdapter extends RecyclerView.Adapter<TransactionRecyclerAdapter.RecyclerViewHolder> {
+
+    List<String> category_names;
+    public TransactionRecyclerAdapter(List<String> category_names){
+
+        this.category_names=category_names;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_category,parent,false);
+        RecyclerViewHolder viewHolder=new RecyclerViewHolder(view);
 
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+
+        holder.category.setText(category_names.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return category_names.size();
+    }
+
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder{
+
+        TextView category;
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+                 category= (TextView)itemView.findViewById(R.id.category_name);
+
+        }
     }
 }
